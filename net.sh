aed0de79814713132df0f91e846ca4850d217664
zheking <zheking@github.com>
2022-07-29 16:32:12 -0700

    improve net.sh

--- net.sh
+++ net.sh
@@ -1,22 +1,58 @@
 #!/bin/bash
 
-
+PROGRAM=$0
 CMDS=
+
+match_cmd() {
+local cmd=""
+local stop=""
+while [ -n "$1" -a -z "$stop" ]
+do
+case $1 in
+    --)
+    stop="yes"
+    ;;
+    *)
+    cmd="${cmd}_$1"
+    declare -f ${cmd} > /dev/null && break
+    ;;
+esac
+shift
+done
+declare -f ${cmd} > /dev/null
+if [ $? == 0 ]; then
+   echo "${cmd}"
+   return 0
+fi
+return 1
+}
+
+match_args() {
+local cmd=""
+local stop=""
+while [ -n "$1" -a -z "$stop" ]
+do
+case $1 in
+    --)
+    stop="yes"
+    ;;
+    *)
+    cmd="${cmd}_$1"
+    declare -f ${cmd} > /dev/null && break
+    ;;
+esac
+shift
+done
+declare -f ${cmd} > /dev/null
+if [ $? == 0 ]; then
+   echo "$@"
+   return 0
+fi
+return 1
+}
 
 _help() { #Help Information
 local full_cmd=$(basename $PROGRAM)
-local cmd=""
-while [ -n "$1" ]
-do
-    case $1 in
-        *)
-        declare -f ${cmd}_$1 > /dev/null || break
-        cmd="${cmd}_$1"
-        full_cmd=${full_cmd}_$1
-        ;;
-    esac
-    shift
-done
 if [ -n "${cmd}" ];then
     echo "${full_cmd} [options]" | sed -n -e "s/_/ /pg"
 else
@@ -29,7 +65,7 @@ EOF
 
 for n in $CMDS
 do
-    local ok=$(echo $n | sed -n -e "s/^\(${cmd}_-\{0,1\}[a-z]*\$\)/\1/p")
+    local ok=$(echo $n | sed -n -e "s/^\(${cmd}_-\{0,1\}[a-z_]*\$\)/\1/p")
     if [ -z "$ok" ];then
         continue
     fi
@@ -48,13 +84,33 @@ ip add del $n dev $dev
 done
 }
 
-_setips() {
+_set_ips() {
 local dev=$1
-local cnt=2
-while [ $cnt -le 22 ]
+local first_ip=$2
+local idx=0
+local cnt=$3
+
+if [ -z "$dev" -o -z "${first_ip}" ]; then
+    echo "set ipds <dev> <first ipv4 address> [how many ips]"
+    exit 1
+fi
+local subnet=$(echo ${first_ip} | sed -n -e "s/^[ \t]*\([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.\)[0-9]\{1,3\}[ \t]*/\1/p")
+local host=$(echo ${first_ip} | sed -n -e "s/^[ \t]*[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.\([0-9]\{1,3\}\)[ \t]*/\1/p")
+
+if [ -z "${subnet}" -o -z "${host}" ]; then
+    echo "set ipds <dev> <first ipv4 address> [how many ips]"
+    exit 1
+fi
+
+ip add add ${subnet}${host}/24 dev ${dev}
+let host=host+1
+let idx=idx+1
+
+while [ $idx -le $cnt ]
 do
-ip add add 1.1.1.$cnt/24 dev $dev
-let cnt=cnt+1
+ip add add ${subnet}${host}/24 dev ${dev}
+let host=host+1
+let idx=idx+1
 done
 }
 
@@ -70,7 +126,7 @@ let cnt=cnt+1
 done
 }
 
-_client() {
+_udp_client() {
    local src_ip=$1
    local src_port=$2
    local dest_ip=$3
@@ -78,53 +134,64 @@ _client() {
 
    if [ -z "$src_ip" -o -z "$src_port" -o -z "$dest_ip" -o -z "$dest_port" ];then
       echo "Usage:"
-      echo "   client <src ip> <src port> <dest ip> <dest port> [tcp|udp]"
+      echo "   client <src ip> <src port> <dest ip> <dest port>"
       exit 1
    fi
-   case $5 in
-      tcp)
-         nc  -s $src_ip -p $src_port $dest_ip $dest_port
-      ;;
-      udp|*)
-         nc  -u -s $src_ip -p $src_port $dest_ip $dest_port
-      ;;
-   esac
+   nc  -u -s $src_ip -p $src_port $dest_ip $dest_port
 }
 
-_server() {
+_udp_server() {
    local dest_ip=$1
    local dest_port=$2
 
    if [ -z "$dest_ip" -o -z "$dest_port" ];then
       echo "Usage:"
-      echo "   server <listen ip> <listen port> [tcp|udp]"
+      echo "   server <listen ip> <listen port>"
       exit 1
    fi
-   case "$3" in
-      tcp)
-         nc  -l $dest_ip $dest_port
-      ;;
-      udp|*)
-         nc  -u -l $dest_ip $dest_port
-      ;;
-   esac
+   nc  -u -l $dest_ip $dest_port
+}
+
+_tcp_client() {
+   local src_ip=$1
+   local src_port=$2
+   local dest_ip=$3
+   local dest_port=$4
+
+   if [ -z "$src_ip" -o -z "$src_port" -o -z "$dest_ip" -o -z "$dest_port" ];then
+      echo "Usage:"
+      echo "   client <src ip> <src port> <dest ip> <dest port>"
+      exit 1
+   fi
+   nc  -s $src_ip -p $src_port $dest_ip $dest_port
+}
+
+_tcp_server() {
+   local dest_ip=$1
+   local dest_port=$2
+
+   if [ -z "$dest_ip" -o -z "$dest_port" ];then
+      echo "Usage:"
+      echo "   server <listen ip> <listen port>"
+      exit 1
+   fi
+   nc  -l $dest_ip $dest_port
+}
+
+_client() {
+   _udp_client $@
+}
+
+_server() {
+   _udp_server $@
 }
 
 CMDS=$(declare -F | awk '{print $3}')
-CMD=""
-STOP=""
-while [ -n "$1" -a -z "$STOP" ]
-do
-case $1 in
-    --)
-    STOP="yes"
-    ;;
-    *)
-    declare -f ${CMD}_$1 > /dev/null || break
-    CMD="${CMD}_$1"
-    ;;
-esac
-shift
-done
+CMD="$(match_cmd $@)"
+PARAMS="$(match_args $@)"
 
-"$CMD" $@
+if [ -z "${CMD}" ]; then
+    _help
+    exit 1
+fi
+$CMD ${PARAMS}
